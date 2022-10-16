3569835d45d7b2ffc952928fff083deb574d082f
Carlos A Pardo G <cpardog@hotmail.com>
2022-10-16 18:27:04 -0500

    nuevo cambio

--- ProjectVB/Program.vb
+++ ProjectVB/Program.vb
@@ -1,9 +1,17 @@
 Imports System
 
 Module Program
+    Public suma As Integer
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Bebe hermoso")
+        sumar(7, 9)
         Console.ReadKey()
+
+    End Sub
+    Public Sub sumar(par1 As Integer, par2 As Integer)
+        suma = par1 + par2
+        Console.WriteLine("La suma  esperada de los operandos es {0}", suma)
+
     End Sub
 End Module
