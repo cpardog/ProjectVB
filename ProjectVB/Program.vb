e9be375b85257d8ffc751475c777119a3f4215af
Carlos A Pardo G <cpardog@hotmail.com>
2022-10-16 18:20:08 -0500

    este commit me pudo matar

---
+++ ProjectVB/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Bebe hermoso")
+        Console.ReadKey()
+    End Sub
+End Module
