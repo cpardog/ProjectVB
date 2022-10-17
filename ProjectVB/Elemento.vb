83ca08ab456285f467fe02c7926d85f2143234ac
cpardog <cpardog@hotmail.com>
2022-10-17 18:08:14 -0500

    Definición de clase elemento

---
+++ ProjectVB/Elemento.vb
@@ -0,0 +1,66 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Elemento
+
+    Private _idelemento As Integer
+    Private _tipoElemento As String
+    Private _fabricante As String
+    Private _serial As String
+    Private _color As String
+
+
+    Public Property IdElemento() As Integer
+        Get
+            Return _idelemento
+        End Get
+        Set(ByVal value As Integer)
+            _idelemento = value
+        End Set
+    End Property
+    Public Property TipoElemento() As String
+        Get
+            Return _tipoElemento
+        End Get
+        Set(ByVal value As String)
+            _tipoElemento = value
+        End Set
+    End Property
+
+    Public Property Fabricante() As String
+        Get
+            Return _fabricante
+        End Get
+        Set(ByVal value As String)
+            _fabricante = value
+        End Set
+    End Property
+
+    Public Property Serial() As String
+        Get
+            Return _serial
+        End Get
+        Set(ByVal value As String)
+            _serial = value
+        End Set
+    End Property
+
+    Public Property Color() As String
+        Get
+            Return _color
+        End Get
+        Set(ByVal value As String)
+            _color = value
+        End Set
+    End Property
+    Private _estado As Boolean
+    Public Property Estado() As Boolean
+        Get
+            Return _estado
+        End Get
+        Set(ByVal value As Boolean)
+            _estado = value
+        End Set
+    End Property
+
+
+End Class
